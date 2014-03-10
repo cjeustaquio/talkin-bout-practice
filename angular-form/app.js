94ebf51c80d785c2fcf25ffe66a4c9492221540b
cjeustaquio <cj.eustaquio@solutionset.com>
2014-03-09 23:36:58 -0700

    show validation error msgs on submit

--- angular-form/app.js
+++ angular-form/app.js
@@ -6,7 +6,7 @@ validationApp.controller('mainController', function($scope) {
 
   // function to submit the form after all validation has occurred      
   $scope.submitForm = function(isValid) {
-
+    $scope.submitted = true;
     // check to make sure the form is completely valid
     if (isValid) { 
       alert('our form is amazing');
